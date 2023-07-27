de72ef828baa7ceb625fff80aef4d2542bc4e8cc
unknown <gonza.cunet@outlook.com>
2023-07-27 11:42:26 -0300

    primer cambio

--- index.js
+++ index.js
@@ -25,6 +25,20 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const ulList = document.querySelector(".lista");
+  const liEl = ulList.querySelectorAll("li");
+
+  for (let i = 0; i < liEl.length; i++) {
+    ulList.removeChild(liEl[i]);
+  }
+
+  cosasQueAprendimos.forEach((element) => {
+    const newLi = document.createElement("li");
+    newLi.textContent = element.tema;
+    newLi.className = element.class;
+    ulList.appendChild(newLi);
+  });
+}
 
 main();
